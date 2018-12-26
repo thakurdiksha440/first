0a0bc296b387a9b91b96bf9f7a2735a4881f6cfe
Diksha thakur <tdiksha45@gmail.com>
2018-12-26 19:11:33 +0530

    slider with javascript

--- src/mainpage.js
+++ src/mainpage.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import { element } from 'prop-types';
 
 class Web extends Component {
     constructor(props) {
-        super(props);
-        this.state = {
+        super(props); this.state = {
             product: [
                 {
                     'id': 1,
@@ -12,43 +12,43 @@ class Web extends Component {
                 },
                 {
                     'id': 2,
-                    'url': 'https://images-eu.ssl-images-amazon.com/images/I/41h1DwFsntL._AC_UL260_SR200,260_FMwebp_QL70_.jpg',
+                    'url': 'https://images-eu.ssl-images-amazon.com/images/I/51tuXGAPn-L._AC_UL260_SR200,260_FMwebp_QL70_.jpg',
                     'title': 'photo2'
                 },
                 {
                     'id': 3,
-                    'url': 'https://images-eu.ssl-images-amazon.com/images/I/41h1DwFsntL._AC_UL260_SR200,260_FMwebp_QL70_.jpg',
+                    'url': 'https://images-eu.ssl-images-amazon.com/images/I/31iYvudN26L._AC_UL260_SR200,260_FMwebp_QL70_.jpg',
                     'title': 'photo3'
                 },
-                {
-                    'id': 4,
-                    'url': 'https://images-eu.ssl-images-amazon.com/images/I/41h1DwFsntL._AC_UL260_SR200,260_FMwebp_QL70_.jpg',
-                    'title': 'photo4'
-                },
+
             ],
             cart: []
         }
     }
 
     cartadd = (data) => {
-
-        this.setState({ cart: [...this.state.cart, data] });
+        // add in cart
+        var array = this.state.cart
+        //  search the array value and what the position of array(index)
+        var index = array.indexOf(data)
+        
+        if (index === -1) {
+            this.setState({ cart: [...this.state.cart, data] });
+        }
     }
 
     delete = (data) => {
-      
-        var array = [...this.state.cart]; // make a separate copy of the array
+        var array = this.state.cart
         var index = array.indexOf(data)
+                                            
         if (index !== -1) {
-            array.splice(index, 1);
+            // change the content of an array  by removing  existing element and or adding new element "in sort chnge the nal array"
+            array.splice(index);
             this.setState({ cart: array });
         }
-
     }
 
-
     render() {
-        console.log(this.cart);
         return (
             <div className="row text-center ">
                 <div className="col-8">
@@ -58,7 +58,7 @@ class Web extends Component {
                             this.state.product.map((index, key) => {
                                 return (<div className="col-4" key={key}>
                                     <div className="border mt-4 p-4">
-                                        <img  alt="no img" src={index.url} /><br />
+                                        <img alt="no img" src={index.url} /><br />
                                         <p>{index.title}</p>
                                         <button className="btn-danger" onClick={() => this.cartadd(index)}>Add Cart</button>
                                     </div>
@@ -96,8 +96,6 @@ class Web extends Component {
                             }
                             )}
                     </div>
-
-
 
 
                 </div>
